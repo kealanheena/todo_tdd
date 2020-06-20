78d5ac807a3b24d70354156f85e945ed18446f4a
kealanheena <kealanheena@gmail.com>
2020-06-20 22:51:36 +0100

    Chore: mock http requests and update test to take mock data

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,5 +1,7 @@
 const TodoController = require("../../controllers/todo.controller");
-const TodoModel = require("../../model/todo.model");
+const TodoModel      = require("../../model/todo.model");
+const httpMocks      = require("node-mocks-http");
+const newTodo        = require("../mock-data/new-todo.json");
 
 TodoModel.create = jest.fn();
 
@@ -10,8 +12,13 @@ describe("TodoController", () => {
     });
 
     it("should call TodoModel.create", () => {
-      TodoController.createTodo();
-      expect(TodoModel.create).toBeCalled();
+      let req, res, next;
+      req = httpMocks.createRequest();
+      res = httpMocks.createResponse();
+      next = null;
+      req.body = newTodo;
+      TodoController.createTodo(req, res, next);
+      expect(TodoModel.create).toBeCalledWith(newTodo);
     })
   });
 });
