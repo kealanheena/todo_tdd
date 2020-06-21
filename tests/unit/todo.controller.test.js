e90d2d08ee05fd7af651a0e98e48cd3ee9b5be43
kealanheena <kealanheena@gmail.com>
2020-06-21 22:05:54 +0100

    Chore: add test for recieving correct status code

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -6,19 +6,31 @@ const newTodo        = require("../mock-data/new-todo.json");
 TodoModel.create = jest.fn();
 
 describe("TodoController", () => {
+
+  let req, res, next;
+
+  beforeEach(() => {
+    req = httpMocks.createRequest();
+    res = httpMocks.createResponse();
+    next = null;
+  })
+
   describe("#createTodo", () => {
     it("should have a createTodo function", () => {
       expect(typeof TodoController.createTodo).toBe("function");
     });
 
     it("should call TodoModel.create", () => {
-      let req, res, next;
-      req = httpMocks.createRequest();
-      res = httpMocks.createResponse();
-      next = null;
       req.body = newTodo;
       TodoController.createTodo(req, res, next);
       expect(TodoModel.create).toBeCalledWith(newTodo);
-    })
+    });
+
+    it("should return a 201 response code", () => {
+      req.body = newTodo;
+      TodoController.createTodo(req, res, next);
+      expect(res.statusCode).toBe(201);
+      expect(res._isEndCalled()).toBeTruthy;
+    });
   });
 });
