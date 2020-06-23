9acf4bbc49fe2ad44549d1d993f3bd2a300636a5
kealanheena <kealanheena@gmail.com>
2020-06-23 21:00:26 +0100

    Chore: add server file and do first manual test using postman

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const todoRoutes = require("./routes/todo.routes");
 const app = express();
 const mongodb = require("./mongodb/mongodb.connect");
-const PORT = 3000;
 
 mongodb.connect();
 
@@ -13,9 +12,5 @@ app.use("/todos", todoRoutes);
 app.get('/', (rq, res) => {
   res.json("hello")
 })
-
-// app.listen(PORT, () => {
-//   console.log(`server is running on port ${PORT}`)
-// })
 
 module.exports = app;
